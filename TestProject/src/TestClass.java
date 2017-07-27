4e70dd86fbd29b74626cc6a2cc44d7fd4ea2f842
Makarand Thorat <Makarand.Thorat@ecommera.com>
2017-07-27 11:56:48 +0530

    Initial commit

---
+++ TestProject/src/TestClass.java
@@ -0,0 +1,69 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.commons.configuration.Configuration;
+
+import de.hybris.platform.core.Registry;
+import de.hybris.platform.servicelayer.config.ConfigurationService;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		ConfigurationService configurationService = Registry.getApplicationContext().getBean(ConfigurationService.class,"configurationService");
+		
+		Configuration configuration = configurationService.getConfiguration();
+		
+		Iterator<String> keys = configuration.getKeys();
+		
+		File file = new File("C:\\apps\\configs.properties");
+		
+		FileWriter fw ;
+		
+		try {
+		
+		fw = new FileWriter(file);
+		
+		while(keys.hasNext())
+			
+		{
+			String key = keys.next();
+			
+
+			
+			String value = configuration.getString(key);
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append(key).append(" = ").append(value).append("\n");
+			
+				try {
+					fw.write(sb.toString());
+				} catch (IOException e) {
+
+					e.printStackTrace();
+				}
+			
+		
+		}
+
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(fw != null) {
+					
+					fw.close();
+				}
+				
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
